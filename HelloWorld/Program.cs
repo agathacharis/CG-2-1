22c76cd887fe3ffa0bd44f4364c8f566f3d96aa2
Agatha Berwick <agatha.berwick@gmail.com>
2018-07-19 12:12:34 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.Write("Type your last name: ");
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello," + myFirstName + " " + myLastName);
+            Console.ReadLine();
+        }
+    }
+}
